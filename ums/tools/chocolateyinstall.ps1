a31b5fd6fcd6cd2d7a53f4cc9192182f3d7deb63
MANICX100 <MANICX100@users.noreply.github.com>
2020-12-12 23:02:33 +0000

    Update to 9.8.2

--- ums/tools/chocolateyinstall.ps1
+++ ums/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'ums'
 $installerType = 'exe' 
 $silentArgs = '/S'
-$url = 'https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/9.8.1/UMS-9.8.1.exe'
-$checksum = '1C2E9821E32DB080E1E6B2C7700D079E763B7369E0232785151ADFCE8F9F5AD8'
+$url = 'https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/9.8.2/UMS-9.8.2.exe'
+$checksum = '521586D1FDF6E61FDD5D540C34AB6FB889C74C7069A232C5B66027B8DE705FB0'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
  
