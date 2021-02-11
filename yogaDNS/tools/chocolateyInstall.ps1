42b2cdff2aa8b660bb6b6c4bc3a22a392e6909d2
Danny J Kendall <d.manicx100@gmail.com>
2021-02-11 09:54:00 +0000

    Updates and adding YogaDNS

---
+++ yogaDNS/tools/chocolateyInstall.ps1
@@ -0,0 +1,16 @@
+﻿$packageName = 'yogadns'
+$installerType = 'exe'
+$silentArgs = '/VERYSILENT'
+$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$url = 'https://yogadns.com/download/YogaDNSSetup.exe'
+$checksum = '6626AA41C3A9358989A81C00F2397F1C774FDEDE524C25271794EB728DB7DB1A'
+$checksumType = 'sha256'
+$validExitCodes = @(0)
+ 
+Install-ChocolateyPackage -PackageName "$packageName" `
+                          -FileType "$installerType" `
+                          -SilentArgs "$silentArgs" `
+                          -Url "$url" `
+                          -ValidExitCodes $validExitCodes `
+                          -Checksum "$checksum" `
+                          -ChecksumType "$checksumType"
