b97b37fa939a41c64886af5f954ce3d578d8c911
Danny J Kendall <d.manicx100@gmail.com>
2021-07-10 22:59:32 +0100

    Update UMS to 10.7.0

--- ums/tools/chocolateyinstall.ps1
+++ ums/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'ums'
 $installerType = 'exe' 
 $silentArgs = '/S'
-$url = 'https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/10.6.0/UMS-10.6.0.exe'
-$checksum = '3B7F749ED8674C739217D60A285796E7A3FA568D3BF4082B053F49CB4FBFE951'
+$url = 'https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/10.7.0/UMS-10.7.0.exe'
+$checksum = '4D73AE055BA6BD71A1D2F8A87113A8D7116578E3122A01693C472DA0308635C3'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
  
