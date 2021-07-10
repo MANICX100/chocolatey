2ce41846494bf11110c061ad28957a5ecd428c8b
Danny J Kendall <d.manicx100@gmail.com>
2021-07-10 20:01:16 +0100

    Updated glasswire to 2.3.323

--- glasswire/tools/chocolateyinstall.ps1
+++ glasswire/tools/chocolateyinstall.ps1
@@ -3,7 +3,7 @@ $installerType = 'exe'
 $silentArgs = '/S'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://download.glasswire.com/GlassWireSetup.exe'
-$checksum = '0581FD6157C14C4B4DD0C770DF2815EF2E9C077D346DC91A4AF0952A934314A2'
+$checksum = '1EC84B2EE76A07358B45DD829A97DD5185F97D1E98DDDE394FA6A152045D4607'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
  
