fa56b5c46a27b089ad849253995416eddcfac945
Danny J Kendall <d.manicx100@gmail.com>
2021-07-25 10:55:15 +0100

    Update UMS to 10.8.2

--- ums/tools/chocolateyinstall.ps1
+++ ums/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'ums'
 $installerType = 'exe' 
 $silentArgs = '/S'
-$url = 'https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/10.8.1/UMS-10.8.1.exe'
-$checksum = '18649DA5BA9DC1A263E90E22CD0E472B8E081063E734269BB1A52BE8F20A7258'
+$url = 'https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/10.8.2/UMS-10.8.2.exe'
+$checksum = 'B9CE853244950E3210408BEBB8A4A6987F213B2FA5C05734D4393F10B69FFC78'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
  
