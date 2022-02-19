74e9fbd8ee7bfb88d13d5e8fd0272d3bcc615329
Dan Kendall <d.manicx100@gmail.com>
2022-02-19 10:37:50 +0000

    Update YogaDNS to 1.3.1

--- yogaDNS/tools/chocolateyInstall.ps1
+++ yogaDNS/tools/chocolateyInstall.ps1
@@ -3,7 +3,7 @@ $installerType = 'exe'
 $silentArgs = '/VERYSILENT'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://yogadns.com/download/YogaDNSSetup.exe'
-$checksum = '29CC7E146C4EBF6010E5F188325551BBC21604B7FD669AD1FD1EA3A1D4EC5814'
+$checksum = '2224B2D7B8FC7782F59EF6CBF8B15F98051309B2C6AB395836563954CE63B1E9'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
  
