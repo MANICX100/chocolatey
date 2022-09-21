3b4a82ef0914cec74ded4fdac2794fa0daff8590
MANICX100 <d.manicx100@gmail.com>
2022-09-21 23:07:57 +0100

    Update UMS to 11.4.1

--- ums/tools/chocolateyinstall.ps1
+++ ums/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'ums'
 $installerType = 'exe' 
 $silentArgs = '/S'
-$url = 'https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/11.4.0/UMS-11.4.0.exe'
-$checksum = 'fb8f9fb10f69bcb8424bd80e5542fa6c1fd361fd5996eeb96fe6d827d9d531b6'
+$url = 'https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/11.4.1/UMS-11.4.1.exe'
+$checksum = '7bc3ef022e6c27a4992bff92416794e2d789afc9f6f7002d9fbf0b6ddb656767'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
  
