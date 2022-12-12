a3acc42db2355c0a26e104cf4f80f7bdbcc10c6f
MANICX100 <d.manicx100@gmail.com>
2022-12-12 23:07:52 +0000

    update ums to 12.0.1

--- ums/tools/chocolateyinstall.ps1
+++ ums/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'ums'
 $installerType = 'exe' 
 $silentArgs = '/S'
-$url = 'https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/11.6.0/UMS-11.6.0.exe'
-$checksum = '8622f8fdc747cac55fbfd413e2074183a4a1c1e9d192a64274e2214166486ba0'
+$url = 'https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/12.0.1/UMS-12.0.1.exe'
+$checksum = 'a58d62f140b7f1a03c542952b152bfdd90a1de777aad5a18b08d80a1dd941431'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
  
