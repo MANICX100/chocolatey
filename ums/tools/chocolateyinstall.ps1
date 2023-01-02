36799bbe334a15b8694c1524028895adeb0b5c46
MANICX100 <d.manicx100@gmail.com>
2023-01-02 08:44:42 +0000

    Update ums to 13.0.1

--- ums/tools/chocolateyinstall.ps1
+++ ums/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'ums'
 $installerType = 'exe' 
 $silentArgs = '/S'
-$url = 'https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/13.0.0/UMS-13.0.0.exe'
-$checksum = '5eeb263d1a783c4ce5ba19c139bbfed2739888d4a5e146e3ae62ca41a9c9673c'
+$url = 'https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/13.0.1/UMS-13.0.1.exe'
+$checksum = '562ccb5023149e66597e2621a2695b22a74ccf6115ff06b0375d3156542ab88d'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
  
