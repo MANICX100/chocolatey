1ef63155b6abe07248a52758eba472d9f509598c
Dan Kendall <d.manicx100@gmail.com>
2023-12-04 23:32:28 +0000

    Update Lagrange to 1.17.5

--- lagrange/tools/chocolateyinstall.ps1
+++ lagrange/tools/chocolateyinstall.ps1
@@ -2,8 +2,8 @@ $packageName = 'Lagrange'
 $installerType = 'exe'
 $silentArgs = '/VERYSILENT'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/skyjake/lagrange/releases/download/v1.17.4/lagrange_v1.17.4_windows-x64_setup.exe'
-$checksum = '4e581f87315a1a45b9ca6bea9d30ad08cf6bd8ebbb18deb262202315b38f9d33'
+$url = 'https://github.com/skyjake/lagrange/releases/download/v1.17.5/lagrange_v1.17.5_windows-x64_setup.exe'
+$checksum = '6b75b8b9f510722d479942b45cbc3ded61c72edb880db7d9db0e5c17d5be4e42'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
  
