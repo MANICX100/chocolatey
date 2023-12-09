6b25bd509aaaa9694dde0b5bf94f2088cb9dd492
Dan Kendall <d.manicx100@gmail.com>
2023-12-09 13:34:40 +0000

    Update ums to 13.8.0

--- ums/tools/chocolateyinstall.ps1
+++ ums/tools/chocolateyinstall.ps1
@@ -1,8 +1,8 @@
 ﻿$packageName = 'ums'
 $installerType = 'exe' 
 $silentArgs = '/S'
-$url = 'https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/13.7.0/UMS-13.7.0.exe'
-$checksum = '6a938d78be7c4f4a86fddc6b2417eaff2758a40c06b4617717ef4a2a0e9536b4'
+$url = 'https://github.com/UniversalMediaServer/UniversalMediaServer/releases/download/13.8.0/UMS-13.8.0.exe'
+$checksum = 'd43c296a410c7755f29c22f61d2d4476d6a53225551d280450b7cf7fd2b86e0c'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
  
