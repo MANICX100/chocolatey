7137156761397a4c069435de7004e818a9f0da57
Dan Kendall <d.manicx100@gmail.com>
2024-01-01 22:17:03 +0000

    Update anytoiso to 3.9.7

--- anytoiso/tools/chocolateyInstall.ps1
+++ anytoiso/tools/chocolateyInstall.ps1
@@ -2,8 +2,8 @@
 $installerType = 'exe'
 $silentArgs = '/VERYSILENT'
 $toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$url = 'https://github.com/crystalidea/anytoiso/releases/download/v3.9.6/anytoiso_setup.exe'
-$checksum = '6DC7574DB3621D4E5B357C4E1A0B819C48BE7568EE8A457DD6EB30AC7511E9D6'
+$url = 'https://github.com/crystalidea/anytoiso-public/releases/download/v3.9.7/anytoiso_setup.exe'
+$checksum = '15a0e378119b5e4d2e45b8f45da6fb835e3df7084fff9242a0436dd3d9c10ffc'
 $checksumType = 'sha256'
 $validExitCodes = @(0)
  
