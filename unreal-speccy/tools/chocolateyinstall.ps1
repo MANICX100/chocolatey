ba80b389ead82344f8d1fcd39262ae05cdbed9ac
Dan Kendall <d.manicx100@gmail.com>
2024-02-13 17:56:37 +0000

    Update Unreal Speccy to 0.0.87

--- unreal-speccy/tools/chocolateyinstall.ps1
+++ unreal-speccy/tools/chocolateyinstall.ps1
@@ -1,16 +1,27 @@
 ﻿$ErrorActionPreference = 'Stop';
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
-$fileLocation = Join-Path $toolsDir 'unreal-speccy.msi'
 
+# Define package parameters
+$packageName = $env:ChocolateyPackageName
+$url = 'https://github.com/djdron/UnrealSpeccyP/releases/download/v0.0.87/unreal-speccy-portable_0.0.87_win32_sdl2_gles2_angle.zip'
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$unzipLocation = Join-Path $toolsDir "$packageName"
+$shortcutName = 'Unreal Speccy'
+
+# Download and unzip the package
 $packageArgs = @{
-  packageName   = $env:ChocolateyPackageName
-  softwareName  = 'Unreal Speccy Portable'
-  file          = $fileLocation
-  fileType      = 'msi'
-  silentArgs    = "/qn /norestart /l*v `"$env:TEMP\$env:ChocolateyPackageName.$env:ChocolateyPackageVersion.log`""
-  validExitCodes= @(0,1641,3010)
+  packageName   = $packageName
+  url           = $url
+  unzipLocation = $unzipLocation
 }
 
-Install-ChocolateyInstallPackage @packageArgs
+Install-ChocolateyZipPackage @packageArgs
 
-Remove-Item -Force $packageArgs.file
+# Assuming 'unreal_speccy_portable.exe' is the main executable, adjust as necessary
+$mainExecutable = Get-ChildItem -Path $unzipLocation -Filter 'unreal_speccy_portable.exe' -Recurse
+
+if ($mainExecutable) {
+  $shortcutPath = Join-Path $env:ProgramData 'Microsoft\Windows\Start Menu\Programs\Chocolatey'
+  Install-ChocolateyShortcut -shortcutFilePath "$shortcutPath\$shortcutName.lnk" -targetPath $mainExecutable.FullName -WorkingDirectory $mainExecutable.DirectoryName
+} else {
+  Write-Host "Main executable not found. Please check the ZIP contents."
+}
