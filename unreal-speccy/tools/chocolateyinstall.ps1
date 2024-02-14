2d8008b969afd7c15b04dead0cc06118e48c0db8
Dan Kendall <d.manicx100@gmail.com>
2024-02-14 08:37:59 +0000

    Update Unreal Speccy to 0.0.87 fix checksum and uninstaller

--- unreal-speccy/tools/chocolateyinstall.ps1
+++ unreal-speccy/tools/chocolateyinstall.ps1
@@ -3,25 +3,28 @@
 # Define package parameters
 $packageName = $env:ChocolateyPackageName
 $url = 'https://github.com/djdron/UnrealSpeccyP/releases/download/v0.0.87/unreal-speccy-portable_0.0.87_win32_sdl2_gles2_angle.zip'
+$checksum = '12BE4B23C45FF740652A858F525672A9A3B78E786096720214DCC6DA27EC4214'
+$checksumType = 'sha256'
 $toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $unzipLocation = Join-Path $toolsDir "$packageName"
 $shortcutName = 'Unreal Speccy'
+$shortcutPath = Join-Path $env:ProgramData 'Microsoft\Windows\Start Menu\Programs\Chocolatey'
 
-# Download and unzip the package
+# Download and unzip the package with checksum verification
 $packageArgs = @{
-  packageName   = $packageName
-  url           = $url
-  unzipLocation = $unzipLocation
+  packageName    = $packageName
+  url            = $url
+  unzipLocation  = $unzipLocation
+  checksum       = $checksum
+  checksumType   = $checksumType
 }
 
 Install-ChocolateyZipPackage @packageArgs
 
-# Assuming 'unreal_speccy_portable.exe' is the main executable, adjust as necessary
-$mainExecutable = Get-ChildItem -Path $unzipLocation -Filter 'unreal_speccy_portable.exe' -Recurse
-
+# Create a shortcut
+$mainExecutable = Get-ChildItem -Path $unzipLocation -Filter '*.exe' | Select-Object -First 1
 if ($mainExecutable) {
-  $shortcutPath = Join-Path $env:ProgramData 'Microsoft\Windows\Start Menu\Programs\Chocolatey'
-  Install-ChocolateyShortcut -shortcutFilePath "$shortcutPath\$shortcutName.lnk" -targetPath $mainExecutable.FullName -WorkingDirectory $mainExecutable.DirectoryName
+  Install-ChocolateyShortcut -shortcutFilePath "$shortcutPath\$shortcutName.lnk" -targetPath $mainExecutable.FullName
 } else {
-  Write-Host "Main executable not found. Please check the ZIP contents."
+  Write-Host "Executable not found. Please check the ZIP contents."
 }
---
+++ unreal-speccy/tools/chocolateyuninstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop';
+
+# Define uninstall parameters
+$packageName = $env:ChocolateyPackageName
+$toolsDir = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$unzipLocation = Join-Path $toolsDir "$packageName"
+$shortcutName = 'Unreal Speccy.lnk' # Ensure this matches the exact name of the shortcut
+$shortcutPath = Join-Path $env:ProgramData 'Microsoft\Windows\Start Menu\Programs\Chocolatey'
+
+# Remove the installed directory
+if (Test-Path $unzipLocation) {
+  Remove-Item -Path $unzipLocation -Recurse -Force
+}
+
+# Remove the shortcut
+$fullShortcutPath = Join-Path $shortcutPath $shortcutName
+if (Test-Path $fullShortcutPath) {
+  Remove-Item -Path $fullShortcutPath -Force
+}
+
+Write-Host "$packageName has been uninstalled."
