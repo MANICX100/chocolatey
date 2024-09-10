53801aafb6f91931c9e5880d980947d2d2ec5ab4
Lemon Lam <alemonmk@users.noreply.github.com>
2024-09-10 20:31:24 +0800

    Remove unused variable in iMazing script

--- imazing/tools/chocolateyinstall.ps1
+++ imazing/tools/chocolateyinstall.ps1
@@ -1,7 +1,6 @@
 ﻿$packageName = 'imazing'
 $installerType = 'exe'
 $silentArgs = '/VERYSILENT /CLOSEAPPLICATIONS /SUPPRESSMSGBOXES /DONTSTART'
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
 $url = 'https://downloads.imazing.com/windows/iMazing/3.0.3.1/iMazing_3.0.3.1.exe'
 $checksum = 'c4e24a6e0159c726169c35fa2c984e491a02746d932b6715492f7920f2e3d460'
 $checksumType = 'sha256'
